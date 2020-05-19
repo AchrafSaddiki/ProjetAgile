e6d23b8c748e230afa86e076864ada6d9ebfd49b
unknown <achraf.saddiki1998@gmail.com>
2020-05-19 22:57:47 +0000

    Block Breaker 2.0

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public GameObject tutoPanel ;
+    public GameObject creditsPanel ;
+
+	public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+    public void ShowTutorial(){
+        tutoPanel.SetActive(true);
+
+
+    }
+    public void HideTutorial(){
+          tutoPanel.SetActive(false);
+
+
+
+
+    }
+    public void ShowCredits(){
+          creditsPanel.SetActive(true);
+
+
+
+    }
+    public void HideCredits(){
+          creditsPanel.SetActive(false);
+
+
+
+
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
