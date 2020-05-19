c262990d3ad459fc4be439ec10005e43322b8299
unknown <achraf.saddiki1998@gmail.com>
2020-05-19 23:58:31 +0000

    Block Breaker 2.0 With Test

---
+++ Assets/Scripts/TestsUnitaires.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestsUnitaires : MonoBehaviour
+{
+	public int NbrBlocks = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NbrBlocks	=GameObject.FindGameObjectWithTag("blocks").GetComponent<Transform>().childCount ;
+    }
+}
